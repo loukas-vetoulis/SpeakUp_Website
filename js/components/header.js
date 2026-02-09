56dcbd9c5153b2f915d561d600c95bee9535515e
loukas-vetoulis <loukasvetoulis@gmail.com>
2026-02-09 20:31:01 +0200

    Add social media icons to the navbar and enhance hover effects

--- js/components/header.js
+++ js/components/header.js
@@ -5,6 +5,18 @@ const HeaderComponent = {
             <div class="search-bar-container">
                 <i class="fas fa-bars hamburger" id="openMenuBtn"></i>
             </div>
+
+            <div class="navbar-socials">
+                <a href="https://www.linkedin.com/company/speakup-aueb-genz/" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="https://www.instagram.com/speakup_auebgenz/" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.tiktok.com/@speakupaueb_genz" target="_blank">
+                    <i class="fab fa-tiktok"></i>
+                </a>
+            </div>
         </div>
 
         <div class="hero-content">
@@ -14,19 +26,13 @@ const HeaderComponent = {
                         <img src="assets/logo/speakup_logo.jpeg" alt="SpeakUp Logo">
                     </a>
                 </div>
-                </div>
             </div>
 
             <div class="hero-text">
                 <h2>Shaping the business leaders of tomorrow</h2>
             </div>
-
-            <div class="hero-socials">
-                <a href="https://www.linkedin.com/company/speakup-aueb-genz/" target="_blank"><i class="fab fa-linkedin"></i></a>
-                <a href="https://www.instagram.com/speakup_auebgenz/" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.tiktok.com/@speakupaueb_genz" target="_blank"><i class="fab fa-tiktok"></i></a>
+            
             </div>
-        </div>
         `;
     }
 };
