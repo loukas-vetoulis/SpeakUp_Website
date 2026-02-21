114f6c78f82bb9c73d1904a42e7b8aff8fc9d9a8
loukas-vetoulis <loukasvetoulis@gmail.com>
2026-02-21 20:30:06 +0200

    Refactor code structure for improved readability and maintainability

--- js/data/carousel.js
+++ js/data/carousel.js
@@ -9,25 +9,25 @@ const CAROUSEL_DATA = [
         title: 'upcoming',    
         highlight: 'EVENTS',    
         url: 'events.html',
-        image: 'assets/carousel/events_photo.jpg'
+        image: 'assets/carousel/events.png'
     },
     { 
         title: 'learn',       
         highlight: 'ABOUT US',  
         url: 'about.html',
-        image: 'assets/carousel/about_photo.jpg'
+        image: 'assets/carousel/about.png'
     },
     // { type: 'team',   title: 'meet the',    highlight: 'TEAM',      url: 'team.html' },
     { 
         title: 'read',        
         highlight: 'BLOGUP',    
         url: 'blog.html',
-        image: 'assets/carousel/blog_photo.jpg'
+        image: 'assets/carousel/blogup.png'
     },
     { 
         title: 'get in',      
         highlight: 'CONTACT',   
         url: 'contact.html',
-        image: 'assets/carousel/contact_photo.jpg'
+        image: 'assets/carousel/contact.png'
     }
 ];
